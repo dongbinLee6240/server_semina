e237af61bd6e23a5e312248d3f99caf7c0cf59e4
이동빈 <108942204+dongbinLee6240@users.noreply.github.com>
2024-07-20 18:14:42 +0900

    07.20 18:14

--- server semina/1week.cpp
+++ server semina/1week.cpp
@@ -5,6 +5,7 @@
 #include <vector>
 
 using namespace std;
+
 #define NUM_THREADS 5
 #define BUFF_SIZE 65536
 
@@ -17,10 +18,12 @@ struct COPY_CHUNK : OVERLAPPED
     DWORD _startOffset;
     DWORD _endOffset;
 
+    //초기화
     COPY_CHUNK(HANDLE hfSrc, HANDLE hfDst, DWORD startOffset, DWORD endOffset)
         :_hfSrc(hfSrc), _hfDst(hfDst), _startOffset(startOffset), _endOffset(endOffset)
     {
-        memset(this, 0, sizeof(COPY_CHUNK));
+        //overlap 구조체 초기화
+        ZeroMemory(this, sizeof(OVERLAPPED));
         _isRead = TRUE; // 읽기 완료
     }
 };
@@ -86,6 +89,8 @@ DWORD WINAPI ThreadProc(LPVOID lpParam)
         pCC->_hfSrc, pCC->_arBuff, BUFF_SIZE, pCC, CopyAPCProc
     );
 
+    cout << "bIsOK: " << bIsOK << endl;
+
     if (!bIsOK) //bIsOK가 false일 경우 -> ReadFileEx 실패
     {
         dwErrCode = GetLastError();
@@ -113,6 +118,7 @@ DWORD WINAPI ThreadProc(LPVOID lpParam)
 
     return (pCC->_errCode == ERROR_SUCCESS) ? 0 : 1;
 }
+
 void _tmain(int argc, _TCHAR* argv[])
 {
     if (argc != 3)
@@ -121,11 +127,14 @@ void _tmain(int argc, _TCHAR* argv[])
         return;
     }
 
+    cout << "Source file path: " << argv[1] << endl;
+    cout << "Destination file path: " << argv[2] << endl;
+
     g_hevExit = CreateEvent(NULL, FALSE, FALSE, NULL);
     SetConsoleCtrlHandler((PHANDLER_ROUTINE)CtrlHandler, TRUE);
 
     HANDLE hSrcFile = CreateFile(argv[1], GENERIC_READ, FILE_SHARE_READ, NULL,
-        OPEN_EXISTING, FILE_FLAG_NO_BUFFERING | FILE_FLAG_OVERLAPPED, NULL);
+        OPEN_EXISTING, FILE_FLAG_OVERLAPPED, NULL);
 
     if (hSrcFile == INVALID_HANDLE_VALUE)
     {
@@ -134,7 +143,7 @@ void _tmain(int argc, _TCHAR* argv[])
     }
 
     HANDLE hDstFile = CreateFile(argv[2], GENERIC_WRITE, FILE_SHARE_WRITE, NULL,
-        OPEN_EXISTING, FILE_FLAG_NO_BUFFERING | FILE_FLAG_OVERLAPPED, NULL);
+        CREATE_ALWAYS, FILE_FLAG_WRITE_THROUGH | FILE_FLAG_OVERLAPPED, NULL);
 
     if (hDstFile == INVALID_HANDLE_VALUE)
     {
@@ -142,16 +151,30 @@ void _tmain(int argc, _TCHAR* argv[])
         CloseHandle(hSrcFile);
         return;
     }
+
     vector<HANDLE> threads(NUM_THREADS);
     vector<PCOPY_CHUNK> chunks(NUM_THREADS);
     DWORD chunkSize = 20; //간격
     DWORD dwErrCode = 0;
+
     for (int i = 0; i < NUM_THREADS; i++)
     {
         DWORD startOffset = i * chunkSize; //0 20 40 60 80
         DWORD endOffset = startOffset + chunkSize;  //20 40 60 80 100
 
+        cout << "hSrcFile: " << hSrcFile << " hDstFile: " << hDstFile << " startOffset: " << startOffset << " endOffset: " << endOffset << endl;
         PCOPY_CHUNK pCC = new COPY_CHUNK(hSrcFile, hDstFile, startOffset, endOffset);
+
+        // Print all relevant members of COPY_CHUNK
+        cout << "pCC의 _hfSrc: " << pCC->_hfSrc << endl;
+        cout << "pCC의 _hfDst: " << pCC->_hfDst << endl;
+        cout << "pCC의 _isRead: " << pCC->_isRead << endl;
+        cout << "pCC의 _errCode: " << pCC->_errCode << endl;
+        cout << "pCC의 _startOffset: " << pCC->_startOffset << endl;
+        cout << "pCC의 _endOffset: " << pCC->_endOffset << endl;
+        cout << "pCC의 Offset: " << pCC->Offset << endl;
+        cout << "pCC의 OffsetHigh: " << pCC->OffsetHigh << endl;
+
         chunks[i] = pCC;
 
         threads[i] = CreateThread(NULL, 0, ThreadProc, pCC, 0, NULL); //비동기 입출력이므로 한 파일에 여러 스레드 접근 가능
@@ -164,6 +187,7 @@ void _tmain(int argc, _TCHAR* argv[])
         CloseHandle(threads[i]);
         delete chunks[i];
     }
+    //열린 파일 닫기
     CloseHandle(hSrcFile);
     CloseHandle(hDstFile);
     CloseHandle(g_hevExit);
